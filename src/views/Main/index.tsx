0a0dc9a5a21274193f80ee4103cfe60a5b9873a2
mafei.mf <mafei.mf@alibaba-inc.com>
2020-11-18 11:09:47 +0800

    fixed a style bug

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,40 +1,32 @@
 import React from 'react';
-import { Row, Col , Skeleton} from 'antd';
-import { observer } from 'mobx-react-lite'; 
+import { Row, Col, Skeleton } from 'antd';
+import { observer } from 'mobx-react-lite';
 import { useStore } from 'store/hook';
 import ViewPanel from 'components/ViewPanel/index';
 import styles from './style.less';
 
 const Main = observer(() => {
+    const clientStore = useStore('clientStore');
+    const { localStream, remoteStreams } = clientStore;
 
-  const clientStore = useStore('clientStore');
-  const { localStream, remoteStreams } = clientStore;
+    if (!localStream) {
+        return <Skeleton paragraph={{ rows: 6 }} active />;
+    }
 
-  if(!localStream) {
-    return(
-      <Skeleton paragraph={{ rows: 6 }} active />
-    )
-  }  
- 
-  return (
-      <Row gutter={[16, 16]} wrap={true}>
-        {
-         localStream? 
-          <Col xs={12} lg={8} xl={8} className={styles.panel}>
-            <ViewPanel type='local' stream={localStream}/>
-          </Col>  
-          :null
-        }
-        {
-         remoteStreams.map((item,index)=>(
-          <Col key={index} xs={12} lg={8} xl={6}  className={styles.panel}>
-            <ViewPanel key={index} type='remote' stream={item}/>
-          </Col>
-         ))
-       }
-      </Row>
-  )
+    return (
+        <Row gutter={[16, 16]} wrap={true}>
+            {localStream ? (
+                <Col xs={12} lg={8} xl={6} className={styles.panel}>
+                    <ViewPanel type="local" stream={localStream} />
+                </Col>
+            ) : null}
+            {remoteStreams.map((item, index) => (
+                <Col key={index} xs={12} lg={8} xl={6} className={styles.panel}>
+                    <ViewPanel key={index} type="remote" stream={item} />
+                </Col>
+            ))}
+        </Row>
+    );
 });
 
-export default Main
-
+export default Main;
